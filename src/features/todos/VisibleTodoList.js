b9814fdaf7fb73a3b3e8d0020ba01cf50231b5b4
uryone <racast324>
2020-12-02 22:23:58 +0900

    Consolidate components into feature folders

--- src/features/todos/VisibleTodoList.js
+++ src/features/todos/VisibleTodoList.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import { createSelector } from '@reduxjs/toolkit'
 import { toggleTodo } from 'features/todos/todosSlice'
-import TodoList from '../components/TodoList'
+import TodoList from './TodoList'
 import { VisibilityFilters } from '../actions'
 
 const selectTodos = state => state.todos
